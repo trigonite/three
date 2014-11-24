6f0c202834d2d98dd677af992ea60a9e52b93e6f
Jamie Greeff <madjam002@yahoo.co.uk>
2014-11-24 16:39:02 +0000

    r69

--- build/three.js
+++ build/three.js
@@ -1,4 +1,4 @@
-// File:src/Three.js
+var self = self || {};// File:src/Three.js
 
 /**
  * @author mrdoob / http://mrdoob.com/
@@ -34728,3 +34728,16 @@ THREE.MorphBlendMesh.prototype.update = function ( delta ) {
 
 };
 
+
+// Export the THREE object for **Node.js**, with
+// backwards-compatibility for the old `require()` API. If we're in
+// the browser, add `_` as a global object via a string identifier,
+// for Closure Compiler "advanced" mode.
+if (typeof exports !== 'undefined') {
+  if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = THREE;
+  }
+  exports.THREE = THREE;
+} else {
+  this['THREE'] = THREE;
+}
